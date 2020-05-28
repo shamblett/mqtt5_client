de356e0c68ffd8ebc8dd7d7fbf12af13e91c99b4
Steve Hamblett <shamblett@gtsdevlan.com>
2020-05-28 11:46:57 +0100

    Clean up the examples

--- example/mqtt5_client.dart
+++ example/mqtt5_client.dart
@@ -7,7 +7,7 @@
 
 /// The mqtt5_client has now been split into a server client [MqttServerClient] and a browser
 /// client [MqttBrowserClient]. Example usage of these two clients are contained in this directory.
-/// Example usage for a server client using secure sockets is in the iot_core.dart file.
+/// Example usage for a server client using secure sockets is in the mqtt5_server_client_secure.dart file.
 ///
 /// Except for connection functionality the behavior of the clients wrt MQTT is the same.
 ///
