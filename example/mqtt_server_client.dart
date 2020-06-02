f8912c5ab67c7fc24e2d6a1fa87e4c7a74857c92
Steve Hamblett <steve.hamblett@linux.com>
2020-06-02 17:55:42 +0100

    Testing against Hive started.

--- example/mqtt_server_client.dart
+++ example/mqtt_server_client.dart
@@ -11,20 +11,20 @@ import 'package:mqtt5_client/mqtt_client.dart';
 import 'package:mqtt5_client/mqtt_server_client.dart';
 
 /// An annotated simple subscribe/publish usage example for mqtt_server_client. Please read in with reference
-/// to the MQTT specification. The example is runnable, also refer to test/mqtt_client_broker_test...dart
+/// to the MQTT 5 specification. The example is runnable, also refer to test/mqtt_client_broker_test...dart
 /// files for separate subscribe/publish tests.
 
 /// First create a client, the client is constructed with a broker name, client identifier
 /// and port if needed. The client identifier (short ClientId) is an identifier of each MQTT
 /// client connecting to a MQTT broker. As the word identifier already suggests, it should be unique per broker.
 /// The broker uses it for identifying the client and the current state of the client. If you don’t need a state
-/// to be hold by the broker, in MQTT 3.1.1 you can set an empty ClientId, which results in a connection without any state.
-/// A condition is that clean session connect flag is true, otherwise the connection will be rejected.
-/// The client identifier can be a maximum length of 23 characters. If a port is not specified the standard port
-/// of 1883 is used.
+/// to be hold by the broker.
+///
+/// If a port is not specified the standard port of 1883 is used.
+///
 /// If you want to use websockets rather than TCP see below.
 
-final client = MqttServerClient('test.mosquitto.org', '');
+final client = MqttServerClient('localhost', '');
 
 Future<int> main() async {
   /// A websocket URL must start with ws:// or wss:// or Dart will throw an exception, consult your websocket MQTT broker
@@ -39,11 +39,11 @@ Future<int> main() async {
   /// list so in most cases you can ignore this.
 
   /// Set logging on if needed, defaults to off
-  client.logging(on: false);
+  client.logging(on: true);
 
   /// If you intend to use a keep alive value in your connect message that is not the default(60s)
   /// you must set it here
-  client.keepAlivePeriod = 20;
+  client.keepAlivePeriod = 60;
 
   /// Add the unsolicited disconnection callback
   client.onDisconnected = onDisconnected;
@@ -66,13 +66,9 @@ Future<int> main() async {
   /// client identifier, any supplied username/password, the default keepalive interval(60s)
   /// and clean session, an example of a specific one below.
   final connMess = MqttConnectMessage()
-      .withClientIdentifier('Mqtt_MyClientUniqueId')
-      .keepAliveFor(20) // Must agree with the keep alive set above or not set
-      .withWillTopic('willtopic') // If you set this you must set a will message
-      // TODO .withWillMessage('My Will message')
-      .startClean() // Non persistent session for testing
-      .withWillQos(MqttQos.atLeastOnce);
-  print('EXAMPLE::Mosquitto client connecting....');
+      .withClientIdentifier('MQTT5 Dart Client')
+      .startClean(); // Non persistent session for testing
+  print('EXAMPLE::Hive client connecting....');
   client.connectionMessage = connMess;
 
   /// Connect the client, any errors here are communicated by raising of the appropriate exception. Note
@@ -87,74 +83,78 @@ Future<int> main() async {
 
   /// Check we are connected
   if (client.connectionStatus.state == MqttConnectionState.connected) {
-    print('EXAMPLE::Mosquitto client connected');
+    print('EXAMPLE::Hive client connected');
   } else {
     /// Use status here rather than state if you also want the broker return code.
     print(
-        'EXAMPLE::ERROR Mosquitto client connection failed - disconnecting, status is ${client.connectionStatus}');
+        'EXAMPLE::ERROR Hive client connection failed - status is ${client.connectionStatus}');
     client.disconnect();
     exit(-1);
   }
 
-  /// Ok, lets try a subscription
-  print('EXAMPLE::Subscribing to the test/lol topic');
-  const topic = 'test/lol'; // Not a wildcard topic
-  client.subscribe(topic, MqttQos.atMostOnce);
-
-  /// The client has a change notifier object(see the Observable class) which we then listen to to get
-  /// notifications of published updates to each subscribed topic.
-  client.updates.listen((List<MqttReceivedMessage<MqttMessage>> c) {
-    final MqttPublishMessage recMess = c[0].payload;
-    final pt =
-        MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
-
-    /// The above may seem a little convoluted for users only interested in the
-    /// payload, some users however may be interested in the received publish message,
-    /// lets not constrain ourselves yet until the package has been in the wild
-    /// for a while.
-    /// The payload is a byte buffer, this will be specific to the topic
-    print(
-        'EXAMPLE::Change notification:: topic is <${c[0].topic}>, payload is <-- $pt -->');
-    print('');
-  });
-
-  /// If needed you can listen for published messages that have completed the publishing
-  /// handshake which is Qos dependant. Any message received on this stream has completed its
-  /// publishing handshake with the broker.
-  client.published.listen((MqttPublishMessage message) {
-    print(
-        'EXAMPLE::Published notification:: topic is ${message.variableHeader.topicName}, with Qos ${message.header.qos}');
-  });
-
-  /// Lets publish to our topic
-  /// Use the payload builder rather than a raw buffer
-  /// Our known topic to publish to
-  const pubTopic = 'Dart/Mqtt_client/testtopic';
-  final builder = MqttClientPayloadBuilder();
-  builder.addString('Hello from mqtt_client');
-
-  /// Subscribe to it
-  print('EXAMPLE::Subscribing to the Dart/Mqtt_client/testtopic topic');
-  client.subscribe(pubTopic, MqttQos.exactlyOnce);
-
-  /// Publish it
-  print('EXAMPLE::Publishing our topic');
-  client.publishMessage(pubTopic, MqttQos.exactlyOnce, builder.payload);
-
-  /// Ok, we will now sleep a while, in this gap you will see ping request/response
-  /// messages being exchanged by the keep alive mechanism.
-  print('EXAMPLE::Sleeping....');
-  await MqttUtilities.asyncSleep(120);
-
-  /// Finally, unsubscribe and exit gracefully
-  print('EXAMPLE::Unsubscribing');
-  client.unsubscribe(topic);
-
-  /// Wait for the unsubscribe message from the broker if you wish.
-  await MqttUtilities.asyncSleep(2);
-  print('EXAMPLE::Disconnecting');
+  sleep(Duration(seconds: 50));
   client.disconnect();
-  return 0;
+  exit(0);
+
+//  /// Ok, lets try a subscription
+//  print('EXAMPLE::Subscribing to the test/lol topic');
+//  const topic = 'test/lol'; // Not a wildcard topic
+//  client.subscribe(topic, MqttQos.atMostOnce);
+//
+//  /// The client has a change notifier object(see the Observable class) which we then listen to to get
+//  /// notifications of published updates to each subscribed topic.
+//  client.updates.listen((List<MqttReceivedMessage<MqttMessage>> c) {
+//    final MqttPublishMessage recMess = c[0].payload;
+//    final pt =
+//        MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+//
+//    /// The above may seem a little convoluted for users only interested in the
+//    /// payload, some users however may be interested in the received publish message,
+//    /// lets not constrain ourselves yet until the package has been in the wild
+//    /// for a while.
+//    /// The payload is a byte buffer, this will be specific to the topic
+//    print(
+//        'EXAMPLE::Change notification:: topic is <${c[0].topic}>, payload is <-- $pt -->');
+//    print('');
+//  });
+//
+//  /// If needed you can listen for published messages that have completed the publishing
+//  /// handshake which is Qos dependant. Any message received on this stream has completed its
+//  /// publishing handshake with the broker.
+//  client.published.listen((MqttPublishMessage message) {
+//    print(
+//        'EXAMPLE::Published notification:: topic is ${message.variableHeader.topicName}, with Qos ${message.header.qos}');
+//  });
+//
+//  /// Lets publish to our topic
+//  /// Use the payload builder rather than a raw buffer
+//  /// Our known topic to publish to
+//  const pubTopic = 'Dart/Mqtt_client/testtopic';
+//  final builder = MqttClientPayloadBuilder();
+//  builder.addString('Hello from mqtt_client');
+//
+//  /// Subscribe to it
+//  print('EXAMPLE::Subscribing to the Dart/Mqtt_client/testtopic topic');
+//  client.subscribe(pubTopic, MqttQos.exactlyOnce);
+//
+//  /// Publish it
+//  print('EXAMPLE::Publishing our topic');
+//  client.publishMessage(pubTopic, MqttQos.exactlyOnce, builder.payload);
+//
+//  /// Ok, we will now sleep a while, in this gap you will see ping request/response
+//  /// messages being exchanged by the keep alive mechanism.
+//  print('EXAMPLE::Sleeping....');
+//  await MqttUtilities.asyncSleep(120);
+//
+//  /// Finally, unsubscribe and exit gracefully
+//  print('EXAMPLE::Unsubscribing');
+//  client.unsubscribe(topic);
+//
+//  /// Wait for the unsubscribe message from the broker if you wish.
+//  await MqttUtilities.asyncSleep(2);
+//  print('EXAMPLE::Disconnecting');
+//  client.disconnect();
+//  return 0;
 }
 
 /// The subscribed callback
