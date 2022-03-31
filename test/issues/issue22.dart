af43bae407094abf0b5f188f358309ea0f83c536
Steve Hamblett <shamblett@gtsdevlan.com>
2022-03-31 07:14:56 +0100

    Issue 22

---
+++ test/issues/issue22.dart
@@ -0,0 +1,17 @@
+/*
+ * Package : mqtt_client
+ * Author : S. Hamblett <steve.hamblett@linux.com>
+ * Date   : 31/05/2017
+ * Copyright :  S.Hamblett
+ */
+
+import 'dart:async';
+import 'package:mqtt5_client/mqtt5_client.dart';
+import 'package:mqtt5_client/mqtt5_server_client.dart';
+import 'package:test/test.dart';
+
+Future<int> main() async {
+  test('Option update', () async {});
+
+  return 0;
+}
