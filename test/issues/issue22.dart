8eb71c0409f5d6b1742dbec07f58debc02cffd1a
Steve Hamblett <shamblett@gtsdevlan.com>
2022-03-31 08:54:12 +0100

    Issue 22

--- test/issues/issue22.dart
+++ test/issues/issue22.dart
@@ -11,7 +11,32 @@ import 'package:mqtt5_client/mqtt5_server_client.dart';
 import 'package:test/test.dart';
 
 Future<int> main() async {
-  test('Option update', () async {});
+  test('Option update', () {
+    final option = MqttSubscriptionOption();
+    option.retainHandling = MqttRetainHandling.doNotSendRetained;
+    expect(option.retainHandling, MqttRetainHandling.doNotSendRetained);
+  });
+
+  test('Option update in subscription', () {
+    final option = MqttSubscriptionOption();
+    option.retainHandling = MqttRetainHandling.doNotSendRetained;
+    expect(option.retainHandling, MqttRetainHandling.doNotSendRetained);
+    final sub = MqttSubscription(MqttSubscriptionTopic("tinydb"), option);
+    expect(sub.option!.retainHandling, MqttRetainHandling.doNotSendRetained);
+  });
+
+  test('Subscribe with option', () async {
+    final client = MqttServerClient.withPort(
+        'test.mosquitto.org', 'client-id-123456789', 1883);
+    client.autoReconnect = true;
+    client.logging(on: true);
+    await client.connect();
+    final option = MqttSubscriptionOption();
+    option.retainHandling = MqttRetainHandling.doNotSendRetained;
+    expect(option.retainHandling, MqttRetainHandling.doNotSendRetained);
+    final sub = MqttSubscription(MqttSubscriptionTopic("tinydb"), option);
+    client.subscribeWithSubscription(sub);
+  });
 
   return 0;
 }
