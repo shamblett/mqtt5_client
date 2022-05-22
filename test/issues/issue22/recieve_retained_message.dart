ca33886206b08d7efbc8df4759d9d4d6d1ba1eae
Steve Hamblett <steve.hamblett@linux.com>
2022-05-22 11:07:49 +0100

    Issue 22

--- test/issues/issue22/recieve_retained_message.dart
+++ test/issues/issue22/recieve_retained_message.dart
@@ -5,9 +5,9 @@ import 'package:typed_data/typed_buffers.dart';
 import 'package:test/test.dart';
 
 Future<int> main() async {
-  recieveRetainedMessageTest(int retainDelay) async {
+  recieveRetainedMessageTestNoDelay() async {
     final client = MqttServerClient("test.mosquitto.org", "");
-
+    client.logging(on: false);
     String topic = "iWantToBeRetained";
     var sendData = Uint8Buffer();
     sendData.add(255); //Super unique data
@@ -17,8 +17,45 @@ Future<int> main() async {
     print("Publishing Message: $sendData at $topic");
     client.publishMessage(topic, MqttQos.exactlyOnce, sendData, retain: true);
 
-    print("Waiting for $retainDelay seconds before subscribing");
-    await MqttUtilities.asyncSleep(retainDelay);
+    print("Waiting for 0 seconds before subscribing");
+    await MqttUtilities.asyncSleep(0);
+
+    client.subscribe(topic, MqttQos.exactlyOnce);
+    client.updates.listen((List<MqttReceivedMessage<MqttMessage>> c) {
+      final data = (c[0].payload as MqttPublishMessage).payload.message!;
+      print("Recieved Message: ${data} at ${c[0].topic}");
+      if (c[0].topic == topic) {
+        recvData = data;
+      }
+    });
+    // Give some time to the broker for a response
+    await MqttUtilities.asyncSleep(1);
+    if (recvData == null) {
+      print("*** ERROR *** - Nothing was recieved!");
+      client.disconnect();
+      return -1;
+    }
+
+    expect(recvData, sendData);
+
+    client.unsubscribeStringTopic(topic);
+    client.disconnect();
+  }
+
+  recieveRetainedMessageTestDelay() async {
+    final client = MqttServerClient("test.mosquitto.org", "");
+    client.logging(on: false);
+    String topic = "iWantToBeRetained";
+    var sendData = Uint8Buffer();
+    sendData.add(255); //Super unique data
+    Uint8Buffer? recvData;
+
+    await client.connect();
+    print("Publishing Message: $sendData at $topic");
+    client.publishMessage(topic, MqttQos.exactlyOnce, sendData, retain: true);
+
+    print("Waiting for 1 seconds before subscribing");
+    await MqttUtilities.asyncSleep(1);
 
     client.subscribe(topic, MqttQos.exactlyOnce);
     client.updates.listen((List<MqttReceivedMessage<MqttMessage>> c) {
@@ -43,9 +80,11 @@ Future<int> main() async {
     client.disconnect();
   }
 
-  test("Get a retained message right after its published", () => recieveRetainedMessageTest(0));
+  test("Get a retained message right after its published",
+      () => recieveRetainedMessageTestNoDelay());
 
-  test("Get a retained message after a delay", () => recieveRetainedMessageTest(1));
+  test("Get a retained message after a delay of 1 second",
+      () => recieveRetainedMessageTestDelay());
 
   return 0;
 }
