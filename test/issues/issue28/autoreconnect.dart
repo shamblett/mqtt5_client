1cd807c79289fb2fbdf238bd1ea43c4cc5cbeecc
Steve Hamblett <steve.hamblett@linux.com>
2022-06-26 10:38:26 +0100

    Autoreconnect test

--- test/issues/issue28/autoreconnect.dart
+++ test/issues/issue28/autoreconnect.dart
@@ -6,65 +6,40 @@
  */
 
 import 'dart:async';
-import 'dart:convert';
 import 'package:mqtt5_client/mqtt5_client.dart';
 import 'package:mqtt5_client/mqtt5_server_client.dart';
 import 'package:test/test.dart';
 
 Future<int> main() async {
-  test('Should maintain subscriptions after autoReconnect', () async {
-    final client = MqttServerClient.withPort(
-        'test.mosquitto.org', 'client-id-123456789', 1883);
+  test('Should resume persistent session after auto reconnect', () async {
+    final client = MqttServerClient.withPort('localhost', 'SJHIssueRx', 1883);
     client.autoReconnect = true;
-    client.logging(on: true);
-    const topic = 'xd/+';
+    client.logging(on: false);
+    const topic = 'counter';
 
-    // Subscribe callback, we do the auto reconnect when we know we have subscribed
-    // second time is from the resubscribe so re publish.
-    var ignoreSubscribe = false;
-    void subCB(subTopic) async {
-      if (ignoreSubscribe) {
-        print(
-            'ISSUE: Received re-subscribe callback for our topic - re publishing');
-        client.publishMessage('xd/light', MqttQos.exactlyOnce,
-            (MqttPayloadBuilder()..addUTF8String('xd')).payload);
-        return;
-      }
-      if (topic == subTopic.topic.rawTopic) {
-        print(
-            'ISSUE: Received subscribe callback for our topic - auto reconnecting');
-        client.doAutoReconnect(force: true);
-      } else {
-        print('ISSUE: Received subscribe callback for unknown topic $subTopic');
-      }
-      ignoreSubscribe = true;
-      print('ISSUE: Exiting subscribe callback');
-    }
-
-    // Main test starts here
-    print('ISSUE: Main test start');
-    client.onSubscribed = subCB; // Subscribe callback
     print('ISSUE: Connecting');
     await client.connect();
-    client.subscribe(topic, MqttQos.exactlyOnce);
 
-    // Now publish the message
-    print('ISSUE: Publishing');
-    client.publishMessage('xd/light', MqttQos.exactlyOnce,
-        (MqttPayloadBuilder()..addUTF8String('xd')).payload);
+    // Subscribe to counter, Qos 1
+    client.subscribe(topic, MqttQos.atLeastOnce);
+    print(
+        'EXAMPLE:: Sleeping to allow the subscription acknowledges to be received....');
+    await MqttUtilities.asyncSleep(2);
 
-    // Listen for our responses.
-    print('ISSUE: Listening >>>>');
-    final stream = client.updates.expand((event) sync* {
-      for (var e in event) {
-        MqttPublishMessage message = e.payload;
-        yield utf8.decode(message.payload.message);
+    // Listen for the counter messages
+    client.updates.listen((List<MqttReceivedMessage<MqttMessage>> c) {
+      final recMess = c[0].payload as MqttPublishMessage;
+      final payload = recMess.payload.message;
+      if (payload != null) {
+        final counterValue = payload[0];
+        print('ISSUE::Change notification:: counter received is $counterValue');
+      } else {
+        print('ISSUE - ERROR payload is null');
       }
-    }).timeout(Duration(seconds: 25));
+    });
 
-    expect(await stream.first, equals('xd'));
     print('ISSUE: Test complete');
-  });
+  }, timeout: Timeout(Duration(seconds: 40)));
 
   return 0;
 }
---
+++ test/issues/issue28/generator.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:mqtt5_client/mqtt5_client.dart';
+import 'package:mqtt5_client/mqtt5_server_client.dart';
+
+const hostName = 'localhost';
+
+Future<int> main() async {
+  final client = MqttServerClient.withPort(hostName, 'SJHIssueTx', 1883);
+  client.logging(on: false);
+  const topic = 'counter';
+
+  print('ISSUE:: client connecting....');
+  try {
+    await client.connect();
+  } on Exception catch (e) {
+    print('EXAMPLE::client exception - $e');
+    client.disconnect();
+  }
+
+  /// Check we are connected
+  if (client.connectionStatus!.state == MqttConnectionState.connected) {
+    print('ISSUE:: client connected');
+  } else {
+    print(
+        'ISSUE::ERROR client connection failed - disconnecting, state is ${client.connectionStatus!.state}');
+    client.disconnect();
+    exit(-1);
+  }
+
+  final builder = MqttPayloadBuilder();
+  for (var x = 1; x < 50; x++) {
+    await MqttUtilities.asyncSleep(1);
+    builder.addInt(x);
+    print('ISSUE:: Publishing counter value $x');
+    client.publishMessage(topic, MqttQos.atLeastOnce, builder.payload!);
+  }
+
+  await MqttUtilities.asyncSleep(2);
+  print('ISSUE::Disconnecting');
+  client.disconnect();
+  return 0;
+}
