e71426f2144a0403dcf2787bea090ce1d87a9b20
Steve Hamblett <steve.hamblett@linux.com>
2025-02-23 10:40:18 +0000

    issue 134

---
+++ test/issue134/issue_134.dart
@@ -0,0 +1,45 @@
+import 'package:mqtt5_client/mqtt5_client.dart' as mqtt;
+import 'package:mqtt5_client/mqtt5_server_client.dart' as mqtt;
+
+Future<void> main() async {
+  final client = mqtt.MqttServerClient.withPort(
+    'wss://test.mosquitto.org/mqtt',
+    'test-b',
+    8081,
+  )
+    ..useWebSocket = true
+    ..connectionMessage = mqtt.MqttConnectMessage()
+        .withWillQos(mqtt.MqttQos.atMostOnce)
+        .withWillTopic('mqtt5_client/test/disconnect_with_will')
+        .withWillPayload(
+      (() {
+        final builder = mqtt.MqttPayloadBuilder();
+        builder.addString('TEST DISCONNECTED');
+        return builder.payload!;
+      })(),
+    )
+    ..disconnectMessage =
+    mqtt.MqttDisconnectMessage()
+        .withReasonCode(mqtt.MqttDisconnectReasonCode.disconnectWithWillMessage)
+    ..onConnected = () {
+      print('onConnected');
+    }
+    ..onDisconnected = () {
+      print('onDisconnected');
+    };
+
+  await client.connect();
+
+  final id = client.publishMessage(
+    'mqtt5_client/test/disconnect_with_will',
+    mqtt.MqttQos.atMostOnce,
+    (() {
+      final builder = mqtt.MqttPayloadBuilder();
+      builder.addString('TEST CONNECTED AT ${DateTime.now()}');
+      return builder.payload!;
+    })(),
+  );
+  print('Published: $id');
+
+  client.disconnect();
+}
