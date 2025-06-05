df78524a8f08ab26470d9c952ddfce0ec88f348a
Steve Hamblett <steve.hamblett@linux.com>
2025-06-05 11:39:35 +0100

    Issue 165 - Fix

--- lib/src/mqtt_client.dart
+++ lib/src/mqtt_client.dart
@@ -628,12 +628,15 @@ class MqttClient {
     }
   }
 
-  /// Turn on logging, true to start, false to stop
-  void logging({required bool on}) {
+  /// Turn on logging, true to start, false to stop.
+  /// Optionally disable publish message payload logging, defaults
+  /// to log payloads, false disables this.
+  void logging({required bool on, bool logPayloads = true}) {
     MqttLogger.loggingOn = false;
     if (on) {
       MqttLogger.loggingOn = true;
     }
+    MqttLogger.logPayloads = logPayloads;
   }
 
   // Process a disconnect message received from the broker.
